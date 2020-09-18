4eb42af14e818030618e47e1573ccb0f161762ad
charlee <programadorch@gmail.com>
2020-09-18 11:20:41 -0500

    se borro el modal para editar usuarios

--- views/modulos/usuarios.php
+++ views/modulos/usuarios.php
@@ -83,7 +83,7 @@
                       <td>'.$valor["ultimo_login"].'</td>
                       <td>
                         <div class="btn-group">
-                          <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                        <button class="btn btn-warning btnEditarUsuario"><i class="fa fa-pencil"></i></button>
                           <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                         </div>
                       </td>
@@ -113,123 +113,135 @@
     =        Modal Cabezera principal        =
     =============================================-->
 
-        <div class="modal-header" style="background: #3c8dbc; color:white">
+    <div id="modalEditarUsuario" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
 
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          
-          <h4 class="modal-title">Agregar Usuario</h4>
+
+          <h4 class="modal-title">Editar usuario</h4>
 
         </div>
 
-        <!--========================================
-    =        Cuerpo del Modal        =
-    =============================================-->
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
 
         <div class="modal-body">
 
           <div class="box-body">
 
-          <!-- Entrada para el Nombre-->  
-
-            <div class="form-group">
-
-              <div class="input-group">
-
-                <span class="input-group-addon"><i class="fa fa-user"></i></span>
-
-                <input type="text"  class="form-control" name="nuevoNombre" placeholder="Ingresar Nombre" require>
-
-              </div>
-
-            </div>
-
-            <!-- Entrada para el Usuario--> 
+            <!-- ENTRADA PARA EL NOMBRE -->
             
             <div class="form-group">
-
+              
               <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
 
-                <span class="input-group-addon"><i class="fa fa-key"></i></span>
-
-                <input type="text"  class="form-control" name="nuevoUsuario" placeholder="Ingresar Usuario" require>
+                <input type="text" class="form-control input-lg" id="editarNombre" name="editarNombre" value="" required>
 
               </div>
 
             </div>
 
-            <!-- Entrada para el Contraseña-->  
+            <!-- ENTRADA PARA EL USUARIO -->
 
-            <div class="form-group">
-
+             <div class="form-group">
+              
               <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
 
-                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-
-                <input type="password"  class="form-control" name="nuevoPassword" placeholder="Ingresar Contraseña" require>
+                <input type="text" class="form-control input-lg" id="editarUsuario" name="editarUsuario" value="" readonly>
 
               </div>
 
             </div>
 
+            <!-- ENTRADA PARA LA CONTRASEÑA -->
 
-            <!-- Entrada para seleccionar perfil-->  
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+
+                <input type="password" class="form-control input-lg" name="editarPassword" placeholder="Escriba la nueva contraseña">
+
+                <input type="hidden" id="passwordActual" name="passwordActual">
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SELECCIONAR SU PERFIL -->
 
             <div class="form-group">
-
+              
               <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-users"></i></span> 
 
-                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                <select class="form-control input-lg" name="editarPerfil">
+                  
+                  <option value="" id="editarPerfil"></option>
 
-                <select class="form-control input-lg" name="nuevoPerfil">
-                  <option value="">Seleccionar Perfil</option>
                   <option value="Administrador">Administrador</option>
+
                   <option value="Especial">Especial</option>
+
                   <option value="Vendedor">Vendedor</option>
+
                 </select>
 
-                
               </div>
 
             </div>
 
-            <!-- Entrada para seleccionar perfil-->  
+            <!-- ENTRADA PARA SUBIR FOTO -->
 
+             <div class="form-group">
+              
+              <div class="panel">SUBIR FOTO</div>
 
+              <input type="file" class="nuevaFoto" name="editarFoto">
 
-            <div class="form-group">
+              <p class="help-block">Peso máximo de la foto 2MB</p>
 
-            <div class="panel">Subir Foto</div>
+              <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail previsualizar" width="100px">
 
-              <input type="file" class="nuevaFoto" name="nuevaFoto">
-
-              <p class="help-block">Peso Maximo de la Foto 200MB</p>
-
-              <img src="views/img/users/default/avatar5.png" class="img.thumbnail previsualizar" width="100px">
+              <input type="hidden" name="fotoActual" id="fotoActual">
 
             </div>
-          
+
+          </div>
 
         </div>
 
-        <!--========================================
-    =        Pie de pagina del modal       =
-    =============================================-->
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
 
         <div class="modal-footer">
 
           <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
-          <button type="submit" class="btn btn-primary">Guardar</button>
-        
+
+          <button type="submit" class="btn btn-primary">Modificar usuario</button>
+
         </div>
-
-        <?php
-
-        $CreateUser = new ControllersUsers();
-        $CreateUser -> ctrCreateUser();
-
-        ?>
-
-        </form>
+    
+      </form>
 
     </div>
 
